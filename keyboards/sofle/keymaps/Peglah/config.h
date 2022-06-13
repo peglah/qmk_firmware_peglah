372cdc98bbf86a71d8c7696c88fc6134dfc8ee5a
Peglah <peglah@gmail.com>
2022-06-13 22:10:10 +0200

    Enable Caps word

--- keyboards/sofle/keymaps/Peglah/config.h
+++ keyboards/sofle/keymaps/Peglah/config.h
@@ -4,3 +4,6 @@
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
+
+// Activate Caps Word by pressing Left Shift + Right Shift
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD 
