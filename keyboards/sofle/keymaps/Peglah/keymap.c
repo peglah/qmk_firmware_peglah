4094019bfafce598e100684a06a6d47cb09c7b11
Peglah <peglah@gmail.com>
2022-06-17 08:25:53 +0200

    fix for macro

--- keyboards/sofle/keymaps/Peglah/keymap.c
+++ keyboards/sofle/keymaps/Peglah/keymap.c
@@ -20,7 +20,7 @@ enum custom_keycodes {
     KC_LSTRT,
     KC_LEND,
     KC_DLINE,
-    BS_WRD = SAFE_RANGE
+    KC_DLWRD = SAFE_RANGE
 };
 
 
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_CAPS,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  BS_WRD,
+  KC_CAPS,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DLWRD,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
   SFT_T(KC_ESC), KC_A, KC_S, KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,  KC_SCLN, KC_QUOT,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,   KC_MNXT, KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
@@ -369,16 +369,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 unregister_code(KC_Z);
             }
             return false;
-        case BS_WRD:
+        case KC_DLWRD:
             if (record->event.pressed) {
-//                register_mods(mod_config(MOD_LCTL));
-//                register_mods(mod_config(MOD_LSFT));
-//                tap_code(KC_LEFT);
-//                unregister_mods(mod_config(MOD_LSFT));
-//                unregister_mods(mod_config(MOD_LCTL));
-//                tap_code(KC_BSPC);
-
-                  SEND_STRING(SS_DOWN(X_LCTRL) SS_TAP(X_BSPC) SS_UP(X_LCTRL));
+                register_mods(mod_config(MOD_LCTL));
+                register_code(KC_BSPC);
+            } else {
+                unregister_mods(mod_config(MOD_LCTL));
+                unregister_code(KC_BSPC);
             }
             return false;
     }
